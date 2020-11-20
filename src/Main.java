a8bb3cf0ce3d447ace33fa7405c5cfecbb9da9ed
sudesh <sudeshkagrawal@users.noreply.github.com>
2020-11-20 13:04:07 -0600

    Updated .gitignore

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,14 @@ public class Main
 	{
 		String outputFolder = "./out/production/NursingHomesTestingStrategy/";
 		// create nursing network
-		int s = 100;
+		int s = 12;
 		
 //		// complete graph
 //		String networkName = "completegraph_staff"+s;
 //		String graphOutputFile = outputFolder + "completegraph_staff"+s+"_graph.txt";
 //		graph network = new graph(networkName);
 //		network.initializeAsCompleteGraph(s, 2);
-//		network.writeNetworkToFile(graphOutputFile, true);
+//		network.writeNetworkToFile(graphOutputFile, false);
 		
 		// neighboring graph
 		int degree = 20;
@@ -32,43 +32,51 @@ public class Main
 		for (int i=0; i<offsets.length; i++)
 			offsets[i] = i+1;
 		network.initializeAsCirculantGraph(s, offsets,2);
-		network.writeNetworkToFile(graphOutputFile, true);
+		network.writeNetworkToFile(graphOutputFile, false);
 		
 //		// crossing graph
-//		String networkName = "crossinggraph_staff"+s;
+//		int degree = 20;
+//		String networkName = "crossinggraph_staff"+s+"_degree"+degree;
+//		String graphOutputFile = outputFolder + "crossinggraph_staff"+s+"_degree"+degree+"_graph.txt";
 //		graph network = new graph(networkName);
-//		network.initializeAsCompleteGraph(s, 2);
+//		int m = (int) (0.5*degree);
+//		int constant = (int) Math.floor(0.5*s-1);
+//		int[] offsets = new int[m];
+//		for (int i=m-1; i>=0; i--)
+//			offsets[i] = constant-i;
+//		network.initializeAsCirculantGraph(s, offsets, 2);
+//		network.writeNetworkToFile(graphOutputFile, false);
 		
-		// simulation
-		int reps = 50000;
-		int timeStep = 6;
-		double falseNegProb = 0.21;
-		double transmissability = 0.05;
-		int latency = 3;
-		double externalInfectionProb = 0.0001;
-		int[] seed = {2507, 2507, 2101, 1308};
-		List<simulationParameters> listOfParams = new ArrayList<>();
-		simulationParameters p1 = new simulationParameters(networkName, timeStep,
-				reps, falseNegProb, transmissability, latency, externalInfectionProb);
-		listOfParams.add(p1);
-		simulationRuns simulationResults = new simulationRuns();
-		simulationResults.simulationForConditionalProbabilityWithLatency(network, listOfParams, seed);
-
-
-		// disease testing
-		String nursingTestResultsFile = outputFolder + "nursingtestresults_"+ networkName + "_reps" + reps + ".csv";
-		boolean append = true;
-		//int[] k = {3};
-		double alpha = 0.05;
-		int baseSeed = 3567;
-		int randomOrderBaseSeed = 1118;
-		fixedNumberOfTestsPerDay testResults = new fixedNumberOfTestsPerDay();
-		for (int i=1; i<=50; i++)
-		{
-			//testResults.testWithRandomOrder(network, simulationResults, i, r, alpha, baseSeed, randomOrderBaseSeed);
-			testResults.test(network, simulationResults, i, alpha, baseSeed);
-		}
-		//System.out.println(testResults.toString());
-		testResults.writeToCSV(nursingTestResultsFile, append);
+//		// simulation
+//		int reps = 5000;
+//		int timeStep = 6;
+//		double falseNegProb = 0.21;
+//		double transmissability = 0.05;
+//		int latency = 3;
+//		double externalInfectionProb = 0.0001;
+//		int[] seed = {2507, 2507, 2101, 1308};
+//		List<simulationParameters> listOfParams = new ArrayList<>();
+//		simulationParameters p1 = new simulationParameters(networkName, timeStep,
+//				reps, falseNegProb, transmissability, latency, externalInfectionProb);
+//		listOfParams.add(p1);
+//		simulationRuns simulationResults = new simulationRuns();
+//		simulationResults.simulationForConditionalProbabilityWithLatency(network, listOfParams, seed);
+//
+//
+//		// disease testing
+//		String nursingTestResultsFile = outputFolder + "nursingtestresults_"+ networkName + "_reps" + reps + ".csv";
+//		boolean append = true;
+//		//int[] k = {3};
+//		double alpha = 0.05;
+//		int baseSeed = 3567;
+//		int randomOrderBaseSeed = 1118;
+//		fixedNumberOfTestsPerDay testResults = new fixedNumberOfTestsPerDay();
+//		for (int i=1; i<=50; i++)
+//		{
+//			//testResults.testWithRandomOrder(network, simulationResults, i, r, alpha, baseSeed, randomOrderBaseSeed);
+//			testResults.test(network, simulationResults, i, alpha, baseSeed);
+//		}
+//		//System.out.println(testResults.toString());
+//		testResults.writeToCSV(nursingTestResultsFile, append);
 	}
 }
